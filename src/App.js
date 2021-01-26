b438c2ae30c0fa5f9a354cd28f65c83a5817f38d
FugaTakata <maple1213goo@gmail.com>
2021-01-26 16:33:00 +0900

    change page layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-function App() {
+import "bulma/css/bulma.min.css";
+import Menu from "./components/Menu";
+
+export default function App() {
   return (
-    <div>
-    </div>
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-one-fifth">
+            <Menu />
+          </div>
+          <div className="column">
+            <div className="row">map</div>
+            <div className="row">graph</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,7 @@
+export default function Menu() {
+  return (
+    <div className="box sticky">
+      <div className="menu">menu</div>
+    </div>
+  );
+}
