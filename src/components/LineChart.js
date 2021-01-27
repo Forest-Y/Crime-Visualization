594254dc0f845398fb8f35ef12e120825486e8fc
Mori <yuuya15009@gmail.com>
2021-01-27 23:35:57 +0900

    key append

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -56,78 +56,80 @@ const LineChart = ({ crimeData }) => {
                                 {crimeData["years"].map((year, i) => {
                                     let preData
                                     return (
-                                        crimeData[key1][key2]["normalizedValue"][year].map((item, j) => {
-                                            if (year !== "2018" && j === 0) {
-                                                preData = crimeData[key1][key2]["normalizedValue"][String(parseFloat(year) - 1)][11]
-                                            } else {
-                                                preData = crimeData[key1][key2]["normalizedValue"][year][Math.max(0, j - 1)]
-                                            }
-                                            return (
-                                                <g>
-                                                    <g>
-                                                        <line
-                                                            x1={Math.max(0, 40 * (i * 12 + j - 1))}
-                                                            y1={height - preData * (height - 65) - 15}
-                                                            x2={40 * (i * 12 + j)}
-                                                            y2={height - item * (height - 65) - 15}
-                                                            stroke="#888"
+                                        <div key={year}>
+                                            {crimeData[key1][key2]["normalizedValue"][year].map((item, j) => {
+                                                if (year !== "2018" && j === 0) {
+                                                    preData = crimeData[key1][key2]["normalizedValue"][String(parseFloat(year) - 1)][11]
+                                                } else {
+                                                    preData = crimeData[key1][key2]["normalizedValue"][year][Math.max(0, j - 1)]
+                                                }
+                                                return (
+                                                    <g key = {item}>
+                                                        <g>
+                                                            <line
+                                                                x1={Math.max(0, 40 * (i * 12 + j - 1))}
+                                                                y1={height - preData * (height - 65) - 15}
+                                                                x2={40 * (i * 12 + j)}
+                                                                y2={height - item * (height - 65) - 15}
+                                                                stroke="#888"
 
-                                                        />
-                                                    </g>
-                                                    <g
-                                                        key={item}
-                                                        transform={`translate(${40 * (i * 12 + j)}, 0)`}
-                                                    >
-                                                        <ellipse
-                                                            cx={0}
-                                                            cy={height - item * (height - 65) - 15}
-                                                            rx="5"
-                                                            ry="5"
-                                                            fill="black"
-                                                            data-tip={"正規化前の認知件数：" + crimeData[key1][key2]["value"][year][Math.max(0, j - 1)]}></ellipse>
-
-
-                                                        <text
-                                                            x="15"
-                                                            y={height + 15}
-                                                            textAnchor="end"
-                                                            dominantBaseline="central"
-                                                            transform="translate(581, 630) rotate(90)"
+                                                            />
+                                                        </g>
+                                                        <g
+                                                            key={item}
+                                                            transform={`translate(${40 * (i * 12 + j)}, 0)`}
                                                         >
-                                                            {year + "年" + yLavel[j].padStart(2, "0") + "月"}
-                                                        </text>
-                                                    </g>
-                                                    <g
-                                                        key={5000 * i}
-                                                        transform={`translate(0, ${-50 * (i * 12 + j + 1)})`}
-                                                    >
-                                                        <line
-                                                            x1="0"
-                                                            y1={height - 15}
-                                                            x2={width - 200}
-                                                            y2={height - 15}
-                                                            stroke="#888"
-                                                        />
+                                                            <ellipse
+                                                                cx={0}
+                                                                cy={height - item * (height - 65) - 15}
+                                                                rx="5"
+                                                                ry="5"
+                                                                fill="black"
+                                                                data-tip={"正規化前の認知件数：" + crimeData[key1][key2]["value"][year][Math.max(0, j - 1)]}></ellipse>
+
+
+                                                            <text
+                                                                x="15"
+                                                                y={height + 15}
+                                                                textAnchor="end"
+                                                                dominantBaseline="central"
+                                                                transform="translate(581, 630) rotate(90)"
+                                                            >
+                                                                {year + "年" + yLavel[j].padStart(2, "0") + "月"}
+                                                            </text>
+                                                        </g>
+                                                        <g
+                                                            key={5000 * i}
+                                                            transform={`translate(0, ${-50 * (i * 12 + j + 1)})`}
+                                                        >
+                                                            <line
+                                                                x1="0"
+                                                                y1={height - 15}
+                                                                x2={width - 200}
+                                                                y2={height - 15}
+                                                                stroke="#888"
+                                                            />
+                                                            <text
+                                                                x="-5"
+                                                                y={height - 15}
+                                                                textAnchor="end"
+                                                                dominantBaseline="central"
+                                                            >
+                                                                {(0.1 * (j + 1)).toFixed(2)}
+                                                            </text>
+                                                        </g>
                                                         <text
                                                             x="-5"
                                                             y={height - 15}
                                                             textAnchor="end"
                                                             dominantBaseline="central"
                                                         >
-                                                            {(0.1 * (j + 1)).toFixed(2)}
+                                                            {0}
                                                         </text>
                                                     </g>
-                                                    <text
-                                                        x="-5"
-                                                        y={height - 15}
-                                                        textAnchor="end"
-                                                        dominantBaseline="central"
-                                                    >
-                                                        {0}
-                                                    </text>
-                                                </g>
-                                            )
-                                        })
+                                                )
+                                            })}
+                                        </div>
                                     )
                                 })}
                             </g>
