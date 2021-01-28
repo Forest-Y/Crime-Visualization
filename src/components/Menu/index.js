c060e0c09546eb8871a19c314dbed299d86bdc45
FugaTakata <maple1213goo@gmail.com>
2021-01-28 13:34:00 +0900

    fix view

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,22 +5,24 @@ export default function Menu({ typeOfCrime }) {
   const [selected, setSelected] = useRecoilState(selectedCrimeState);
 
   return (
-    <div className="box sticky">
-      <div className="menu">
-        {typeOfCrime.map((crimeName) => (
-          <div className="control" key={crimeName}>
-            <label className="radio">
-              <input
-                type="radio"
-                value={crimeName}
-                checked={selected === crimeName ? "checked" : ""}
-                name="radio"
-                onChange={(e) => setSelected(e.target.value)}
-              />
-              {crimeName}
-            </label>
-          </div>
-        ))}
+    <div className="container">
+      <div className="box">
+        <div className="menu">
+          {typeOfCrime.map((crimeName) => (
+            <div className="control" key={crimeName}>
+              <label className="radio">
+                <input
+                  type="radio"
+                  value={crimeName}
+                  checked={selected === crimeName ? "checked" : ""}
+                  name="radio"
+                  onChange={(e) => setSelected(e.target.value)}
+                />
+                {crimeName}
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
